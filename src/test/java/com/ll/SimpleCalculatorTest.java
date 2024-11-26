9b42d7bc9ad0ac65efd94cb25db69595b7f197cf
TTaiJin <taeparis980110@gmail.com>
2024-11-26 11:28:38 +0900

    assertThat

--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -3,6 +3,7 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,7 +15,7 @@ public class SimpleCalculatorTest {
 
         int rs = simpleCalculator.plus(1, 2);
 
-        assertEquals(3, rs);
+        assertThat(rs).isEqualTo(3);
     }
 
     @Test
@@ -36,4 +37,45 @@ public class SimpleCalculatorTest {
 
         assertEquals(30, rs);
     }
+
+    @Test
+    @DisplayName("2 * 3 = 6")
+    public void t4() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int rs = simpleCalculator.multiply(2, 3);
+
+        assertEquals(6, rs);
+    }
+
+    @Test
+    @DisplayName("5 - 3 = 2")
+    public void t5() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int rs = simpleCalculator.minus(5, 3);
+
+        assertEquals(2, rs);
+    }
+
+    @Test
+    @DisplayName("5 / 3 = 1")
+    public void t6() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int rs = simpleCalculator.divide(5, 3);
+
+        assertEquals(1, rs);
+    }
+
+    @Test
+    @DisplayName("5 % 3 = 2")
+    public void t7() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int rs = simpleCalculator.reminder(5, 3);
+
+        assertEquals(2, rs);
+    }
+
 }
